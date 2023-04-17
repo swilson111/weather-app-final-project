51ce47c7e2c56e3f1da57babc224f79257b0d3a3
swilson111 <108382568+swilson111@users.noreply.github.com>
2023-04-17 10:16:31 +1000

    JS

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=${apiKey}&units=metric`;
+let city = "Melbourne"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
