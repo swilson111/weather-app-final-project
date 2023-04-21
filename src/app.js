a425996bbfd208a4fb389c7bed913041b0b490a9
swilson111 <108382568+swilson111@users.noreply.github.com>
2023-04-21 12:26:06 +1000

    final touches

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ function formatDate(timestamp){
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
-        hours = `0${hours}`;
+        hours = `0${hours}`;}
     let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
@@ -11,12 +11,12 @@ function formatDate(timestamp){
     let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day= days[date.getDay()];
     return `${day} ${hours} ${minutes}`;
-}}
+}
 
-function formatDay(timestamp){
-let date = new Date(timestamp * 1000);
-let day = date.getDay();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 return days [day];
 }
